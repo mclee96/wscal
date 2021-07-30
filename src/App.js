7849883c52d683e83fab3d301b00768509eb1aeb
Matthew Lee <matthew@mac.local>
2021-07-29 20:34:14 -0700

    uploading morph

--- src/App.js
+++ src/App.js
@@ -1,37 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import ch02 from './vocab/ch02.tsv'
-import ch03 from './vocab/ch03.tsv'
-import ch04 from './vocab/ch04.tsv'
-import ch05 from './vocab/ch05.tsv'
-import ch06 from './vocab/ch06.tsv'
-import ch07 from './vocab/ch07.tsv'
-import ch08 from './vocab/ch08.tsv'
-import ch09 from './vocab/ch09.tsv'
-import ch10 from './vocab/ch10.tsv'
-import ch11 from './vocab/ch11.tsv'
-import ch12 from './vocab/ch12.tsv'
-import ch13 from './vocab/ch13.tsv'
-import ch14 from './vocab/ch14.tsv'
-import ch15 from './vocab/ch15.tsv'
-import ch16 from './vocab/ch16.tsv'
-import ch17 from './vocab/ch17.tsv'
-import ch18 from './vocab/ch18.tsv'
-import ch19 from './vocab/ch19.tsv'
-import ch20 from './vocab/ch20.tsv'
-import ch21 from './vocab/ch21.tsv'
-import ch22 from './vocab/ch22.tsv'
-import ch23 from './vocab/ch23.tsv'
-import ch24 from './vocab/ch24.tsv'
-import ch25 from './vocab/ch25.tsv'
-import ch26 from './vocab/ch26.tsv'
-import ch27 from './vocab/ch27.tsv'
-import ch28 from './vocab/ch28.tsv'
-import ch29 from './vocab/ch29.tsv'
-import ch30 from './vocab/ch30.tsv'
-import vocab from './vocab/vocab.tsv'
+import vocab from './vocab.tsv'
+
+const Papa = require('papaparse');
 
 function App() {
+  Papa.parse(vocab, {
+    complete: function(results) {
+      console.log(results);
+    },
+    download: true,
+    header: true
+  });
   return (
     <div className="App">
       <header className="App-header">
