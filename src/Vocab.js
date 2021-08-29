c459b5c3e36187eb279497e47b74f131531950f7
Matthew Lee <matthew@mac.local>
2021-08-28 21:17:22 -0700

    beautiful pseudo-badges

--- src/Vocab.js
+++ src/Vocab.js
@@ -1,43 +1,50 @@
-import './App.css';
 import React from 'react'
 
-import Alert from 'react-bootstrap/Alert'
 import Badge from 'react-bootstrap/Badge'
 import Button from 'react-bootstrap/Button'
-import ButtonGroup from 'react-bootstrap/ButtonGroup'
-import ButtonToolbar from 'react-bootstrap/ButtonToolbar'
 import Col from 'react-bootstrap/Col'
-import Collapse from 'react-bootstrap/Collapse'
 import Container from 'react-bootstrap/Container'
 import Form from 'react-bootstrap/Form'
 import InputGroup from 'react-bootstrap/InputGroup'
-import Offcanvas from 'react-bootstrap/Offcanvas'
 import Row from 'react-bootstrap/Row'
-import Table from 'react-bootstrap/Table'
 import ToggleButton from 'react-bootstrap/ToggleButton'
-import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup'
 
 import { VariableSizeGrid } from 'react-window'
 
 import Data from './backend/Data.js'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { saveAs } from 'file-saver';
 
-
-import {ALL, ADVERB_TYPE, RESULT, LEMMA, CASE, GENDER, NUMBER, MOOD, PERSON, TENSE, VOICE, CHAPTER, GLOSS, REFERENCE, ESV, NA28} from './backend/Filters.js'
-import {PART, NOUN, VERB, ADJECTIVE, PREPOSITION, PRONOUN, ADVERB, CONJUNCTION } from './backend/Filters.js'
+import {LEMMA, CHAPTER, GLOSS, PART } from './backend/Filters.js'
 
 class Vocab extends React.Component {
   constructor(props) {
     super(props)
     this.state = { 
-      vocab: Data.getVocab(),
+      vocab: [],
+      selected: [],
     }
+
+    this.toggleSelect = this.toggleSelect.bind(this)
   }
 
   componentDidMount() {
     Data.loadData().then((vocab) => this.setState({ vocab: vocab }));
+  }
+
+  toggleSelect(vocabIndex) {
+    this.setState((state, props) => { 
+      if (!state.selected.includes(vocabIndex)) {
+        // select vocab
+        return { selected: state.selected.concat([vocabIndex]) }
+      } else {
+        // deselect vocab
+        let sliceIndex = state.selected.indexOf(vocabIndex)
+        return { selected: state.selected.slice(0, sliceIndex)
+                   .concat(state.selected.slice(sliceIndex + 1)) }
+      }
+    })
+    console.log(this.state.selected)
   }
 
   render() {
@@ -46,29 +53,67 @@ class Vocab extends React.Component {
 
     return (
       <Container>
-        <InputGroup size="sm">
-          <Form.Control
-            aria-label="chapter restrictions (e.g. 2 or 2,3 or 2-4)"
-            aria-describedby="basic-addon1"
-            placeholder='e.g. "ch2,10-11,πᾶς,εἰμί". Leave blank for any chapter/word.'
-            onChange={this.updateChapter} />
-        </InputGroup>
-        <VariableSizeGrid 
-          height={500}
-          width={300}
-          columnCount={4}
-          columnWidth={index => columnWidths[index]}
-          rowCount={this.state.vocab.length} 
-          rowHeight={index => 40}>
-          {({ columnIndex, rowIndex, style }) => (
-            <div style={Object.assign({}, style, { textAlign: 'left' })}>
-              {columnIndex === 0 
-                ?  <Badge bg="primary" as="Button" style={{ borderWidth: 'thin' }}>+ add</Badge>
-                : this.state.vocab[rowIndex][vocabHeadings[columnIndex - 1]]
-              }
-            </div>
-          )}
-        </VariableSizeGrid>
+        <Row>
+          <Col>
+            <InputGroup size="sm">
+              <Form.Control
+                aria-label="chapter restrictions (e.g. 2 or 2,3 or 2-4)"
+                aria-describedby="basic-addon1"
+                placeholder='e.g. "ch2,10-11,πᾶς,εἰμί". Leave blank for any chapter/word.'
+                onChange={this.updateChapter} />
+            </InputGroup>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <VariableSizeGrid
+              height={480}
+              width={300}
+              columnCount={4}
+              columnWidth={index => columnWidths[index]}
+              rowCount={this.state.vocab.length}
+              rowHeight={index => 40}>
+              {({ columnIndex, rowIndex, style }) => (
+                <div style={Object.assign({}, style, { textAlign: 'left' })}>
+                  {columnIndex === 0
+                    ? <ToggleButton
+                        id={rowIndex}
+                        type="checkbox"
+                        value={rowIndex}
+                        variant="outline-primary"
+                        size="sm"
+                        checked={this.state.selected.includes(rowIndex)}
+                        onClick={ () => this.toggleSelect(rowIndex) }
+                        text="primary"
+                        style={{ lineHeight: 1, 
+                                 fontSize: '.75em', 
+                                 padding: '.35em .65em',
+                                 fontWeight: 700 }}>
+                        +
+                      </ToggleButton>
+                    : this.state.vocab[rowIndex][vocabHeadings[columnIndex - 1]]
+                  }
+                </div>
+              )}
+            </VariableSizeGrid>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            {this.state.selected.map(index => (
+                <Badge pill
+                  key={index}
+                  className='me-1'
+                  bg="primary"
+                  as="button" 
+                  style={{ borderWidth: 'thin' }}
+                  onClick={ () => this.toggleSelect(index) }>
+                  { this.state.vocab[index][LEMMA] }
+                </Badge>
+              ))
+            }
+          </Col>
+        </Row>
       </Container>
     );
   }
